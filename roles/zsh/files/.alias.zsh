28d287e0111df1990b03f7e42f35a2ae6b8a6992
centsent <centsent@users.noreply.github.com>
2025-02-16 10:58:06 +0800

    fix(zsh): format alias file

--- roles/zsh/files/.alias.zsh
+++ roles/zsh/files/.alias.zsh
@@ -1,36 +1,36 @@
 #!/bin/zsh
 
 # Check if eza is installed, and if it is, set up an alias for it
-if type eza > /dev/null; then
-    alias l="eza --long --color --icons --all"
-    alias ls="eza --grid --color --icons --all"
+if type eza >/dev/null; then
+  alias l="eza --long --color --icons --all"
+  alias ls="eza --grid --color --icons --all"
 fi
 
 # Check if rip is installed, and if it is, set up an alias for it
-if type rip > /dev/null; then
-    alias rm="rip"
+if type rip >/dev/null; then
+  alias rm="rip"
 fi
 
-if type xcp > /dev/null; then
-    alias cp="xcp"
+if type xcp >/dev/null; then
+  alias cp="xcp"
 fi
 
-if type topgrade > /dev/null; then
-    alias tg="topgrade"
+if type topgrade >/dev/null; then
+  alias tg="topgrade"
 fi
 
-if type z > /dev/null; then
-    alias cd="z"
+if type z >/dev/null; then
+  alias cd="z"
 fi
 
-if type bat > /dev/null; then
-    alias cat="bat"
+if type bat >/dev/null; then
+  alias cat="bat"
 fi
 
-if type procs > /dev/null; then
-    alias ps="procs"
+if type procs >/dev/null; then
+  alias ps="procs"
 fi
 
-if type pnpm > /dev/null; then
+if type pnpm >/dev/null; then
   alias pn=pnpm
 fi
