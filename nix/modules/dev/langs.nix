81451ac5c440cd08fb764438bf60bb38de4ac3b3
centsent <centsent@users.noreply.github.com>
2025-07-06 01:09:01 +0800

    feat(nix): update dependencies and add more languages

--- nix/modules/dev/langs.nix
+++ nix/modules/dev/langs.nix
@@ -3,16 +3,20 @@
 {
   home.packages = with pkgs; [
     bun
+    cabal-install
     dotnet-sdk
     flutter
     gcc
     gdb
+    ghc
     gnumake
     go
     lua
+    jdk
     nodejs
     pnpm
     python3
+    ruby
     rustup
   ];
 }
