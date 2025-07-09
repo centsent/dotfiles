c6bbc50d51c7d087fb16429e6479d030068631a0
centsent <2985258+centsent@users.noreply.github.com>
2025-07-09 21:41:33 +0800

    fix: Remove flatpak config from Nix

--- nix/modules/apps/linux-apps.nix
+++ nix/modules/apps/linux-apps.nix
@@ -3,14 +3,10 @@
 {
   imports = [
     ./desktop-entries.nix
-    ./flatpak.nix
     ./hyprland.nix
     ./swaync.nix
     ./waybar.nix
     ./wlogout.nix
     ./wofi.nix
-  ];
-
-  home.packages = with pkgs; [
   ];
 }
