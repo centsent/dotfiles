cbd34582966932b0e8c09068997d2aede4f6adf0
centsent <2985258+centsent@users.noreply.github.com>
2025-07-11 19:03:25 +0800

    refactor(doom-emacs): manage doom-emacs with ansible

--- nix/modules/apps/doom-emacs.nix
+++ nix/modules/apps/doom-emacs.nix
@@ -1,48 +1,27 @@
-{ pkgs, flake, lib, ... }:
+{ pkgs, lib, ... }:
 
-let
-  doom-src = pkgs.fetchFromGitHub {
-    owner = "doomemacs";
-    repo = "doomemacs";
-    rev = "master";
-    hash = "sha256-2KFT9v/PGb2K8Vd2eLAhDFQW6ORQapmQmBXipObpkoo=";
-  };
-in
 {
   home.packages = [ pkgs.emacs-pgtk ];
 
-  home.file.".config/emacs" = {
-    source = doom-src;
-    recursive = true;
-  };
-
-  home.file.".doom.d" = {
-    source = "${flake}/.config/doom.d";
-    recursive = true;
-  };
-
-  home.file.".local/share/applications/emacs-doom.desktop" = lib.mkIf pkgs.stdenv.isLinux {
-    text = ''
-      [Desktop Entry]
-      Name=Emacs (Doom)
-      Comment=Text Editor
-      Exec=${pkgs.emacs-pgtk}/bin/emacs
-      Icon=emacs
-      Terminal=false
-      Type=Application
-      Categories=Development;Editor;
-    '';
-  };
-
-  # home.sessionVariables.PATH = "$HOME/.config/emacs/bin:$PATH";
+  home.file.".local/share/applications/emacs-doom.desktop" =
+    lib.mkIf pkgs.stdenv.isLinux {
+      text = ''
+        [Desktop Entry]
+        Name=Emacs (Doom)
+        Comment=Text Editor
+        Exec=${pkgs.emacs-pgtk}/bin/emacs
+        Icon=emacs
+        Terminal=false
+        Type=Application
+        Categories=Development;Editor;
+      '';
+    };
 
   home.activation = {
-    installAndSync = lib.hm.dag.entryAfter ["writeBoundary"] ''
+    installAndSync = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
       if [ -x "$HOME/.config/emacs/bin/doom" ]; then
         echo "Running 'doom install'..."
-        export DOOMDIR=$HOME/.doom.d
         export PATH=${pkgs.emacs-pgtk}/bin:${pkgs.git}/bin:$PATH
-        export DOOMLOCALDIR=$HOME/.local/share/doom
         $HOME/.config/emacs/bin/doom install
       fi
     '';
--- nix/modules/core/environment.nix
+++ nix/modules/core/environment.nix
@@ -2,11 +2,11 @@
 
 let
   # Dynamically set ANDROID_HOME based on the OS
-  androidHome = if pkgs.stdenv.isDarwin 
-               then "$HOME/Library/Android/Sdk"
-               else "$HOME/Android/Sdk";
-in
-{
+  androidHome = if pkgs.stdenv.isDarwin then
+    "$HOME/Library/Android/Sdk"
+  else
+    "$HOME/Android/Sdk";
+in {
   # --- Environment Variables ---
   home.sessionVariables = {
     BUN_INSTALL = "$HOME/.bun";
@@ -19,6 +19,7 @@ in
     "$HOME/.bun/bin"
     "$HOME/go/bin"
     "$HOME/.pub-cache/bin"
+    "$HOME/.config/emacs/bin"
     "${androidHome}/platform-tools"
     "${androidHome}/tools"
     "${androidHome}/tools/bin"
