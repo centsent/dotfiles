bf29842793982c6445bedb9a1f9e776d6b21c338
centsent <2985258+centsent@users.noreply.github.com>
2025-07-11 15:14:01 +0800

    feat(neovim): Remove `flutter_lookup_cmd` option

--- nix/.config/nvim/lua/plugins/flutter.lua
+++ nix/.config/nvim/lua/plugins/flutter.lua
@@ -1,22 +1,23 @@
 return {
-  "nvim-flutter/flutter-tools.nvim",
-  lazy = false,
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-  },
-  opts = {
-    flutter_lookup_cmd = "which flutter",
-  },
-  keys = {
-    { "<leader>rd", "<cmd>FlutterDevices<cr>", desc = "Brings up a list of connected devices to select from" },
-    {
-      "<leader>re",
-      "<cmd>FlutterEmulators<cr>",
-      desc = "Similar to devices but shows a list of emulators to choose from",
-    },
-    { "<leader>rl", "<cmd>FlutterLogToggle<cr>", desc = "Toggle the log buffer" },
-    { "<leader>rr", "<cmd>FlutterRun<cr>", desc = "Run the current project. Respects config.debugger.enabled setting" },
-    { "<leader>rq", "<cmd>FlutterQuit<cr>", desc = "Ends a running session" },
-    { "<leader>rR", "<cmd>FlutterRestart<cr>", desc = "Restart the current project" },
-  },
+	"nvim-flutter/flutter-tools.nvim",
+	lazy = false,
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
+	keys = {
+		{ "<leader>rd", "<cmd>FlutterDevices<cr>", desc = "Brings up a list of connected devices to select from" },
+		{
+			"<leader>re",
+			"<cmd>FlutterEmulators<cr>",
+			desc = "Similar to devices but shows a list of emulators to choose from",
+		},
+		{ "<leader>rl", "<cmd>FlutterLogToggle<cr>", desc = "Toggle the log buffer" },
+		{
+			"<leader>rr",
+			"<cmd>FlutterRun<cr>",
+			desc = "Run the current project. Respects config.debugger.enabled setting",
+		},
+		{ "<leader>rq", "<cmd>FlutterQuit<cr>", desc = "Ends a running session" },
+		{ "<leader>rR", "<cmd>FlutterRestart<cr>", desc = "Restart the current project" },
+	},
 }
